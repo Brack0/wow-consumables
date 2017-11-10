0c08918b662f20ebb768925951504cbfbe5ec9f3
Brack0 <denis.souron@laposte.net>
2017-11-10 22:56:51 +0100

    rxjs, subscribe and changeDetection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
 import { MatTabsModule } from '@angular/material';
 
@@ -21,6 +21,7 @@ import { SpecializationComponent } from './specialization/specialization.compone
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     MatInputModule,
     BrowserAnimationsModule,
     MatTabsModule
--- src/app/consumable/consumable.component.ts
+++ src/app/consumable/consumable.component.ts
@@ -1,17 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 
 import { Consumable } from '../model/consumable';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
+import '@rxjs';
 
 @Component({
   selector: 'app-consumable',
   templateUrl: './consumable.component.html',
-  styleUrls: ['./consumable.component.scss']
+  styleUrls: ['./consumable.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConsumableComponent implements OnInit {
   @Input() consumable: Consumable;
   @Input() displayMaterial = false;
+  form: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // init form
+    this.form = this.fb.group({
+      wantedNumber: [this.consumable.wantedNumber, [Validators.min(0)]]
+    });
+
+    // subscribe to input value and update with a debounce time of 500ms
+    const wantedNumberControl = this.form.get('wantedNumber');
+    wantedNumberControl.valueChanges
+      .debounceTime(500)
+      .subscribe((n: number) =>
+        this.computeWantedNumber(n, wantedNumberControl)
+      );
+  }
+
+  /**
+   * Either update data or create a message
+   * @param n input value
+   * @param c input control
+   */
+  computeWantedNumber(n: number, c: AbstractControl): void {
+    if (c.touched || c.dirty) {
+      if (c.errors) {
+        // show error on input (see doc on material.angular.io)
+      } else {
+        this.consumable.wantedNumber = n;
+        this.cd.markForCheck();
+      }
+    }
+  }
 }
